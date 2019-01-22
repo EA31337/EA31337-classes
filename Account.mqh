EA31337/EA31337-classes

commit ceaad2bbf8002c671bfb3cfad7f619b22c9f9c4e

Zero division fixes

Account.mqh
@@ -479,7 +479,7 @@ class Account {
    *   The risk higher than 1.0 means that the risk is extremely high.
    */
   double GetRiskMarginLevel(ENUM_ORDER_TYPE _cmd = NULL) {
-    return 1 / AccountAvailMargin() * Convert::ValueToMoney(trades.TotalSL(_cmd));
+    return 1 / fmax(1, AccountAvailMargin() * Convert::ValueToMoney(trades.TotalSL(_cmd)));
   }
 
   /**

Trade.mqh
@@ -234,7 +234,7 @@ class Trade {
     double _avail_amount = _method % 2 == 0 ? this.Account().GetMarginAvail() : this.Account().GetRealBalance();
     if (_method == 0 || _method == 1) {
       _lot_size = this.Market().NormalizeLots(
-        _avail_amount / this.Chart().GetMarginRequired() * _risk_ratio / 100 * _risk_ratio
+        _avail_amount / fmax(0.00001, this.Chart().GetMarginRequired() * _risk_ratio) / 100 * _risk_ratio
       );
     } else {
       double _risk_amount = _avail_amount / 100 * _risk_margin;

commit d7288d02396cea7a291b9e0f8f9425743f5d8e9b

Fixes GetRiskMarginLevel() as per regression in ceaad2b

Account.mqh
@@ -480,7 +480,8 @@ class Account {
    *   The risk higher than 1.0 means that the risk is extremely high.
    */
   double GetRiskMarginLevel(ENUM_ORDER_TYPE _cmd = NULL) {
-    return 1 / fmax(1, AccountAvailMargin() * Convert::ValueToMoney(trades.TotalSL(_cmd)));
+    double _avail_margin = AccountAvailMargin() * Convert::ValueToMoney(trades.TotalSL(_cmd));
+    return _avail_margin > 0 ? 1 / _avail_margin : 0;
   }
 
   /**