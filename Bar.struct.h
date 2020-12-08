1cead7eae71808a2bfa756f63202e472a951bcde
kenorb <kenorb@users.noreply.github.com>
2020-12-08 17:56:07 +0100

    Bar/Chart: Moves bar related structs to Bar.struct.h

---
+++ Bar.struct.h
@@ -0,0 +1,109 @@
+//+------------------------------------------------------------------+
+//|                                                EA31337 framework |
+//|                       Copyright 2016-2020, 31337 Investments Ltd |
+//|                                       https://github.com/EA31337 |
+//+------------------------------------------------------------------+
+
+/*
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * @file
+ * Includes Bar's structs.
+ */
+
+// Includes.
+#include "SerializerNode.enum.h"
+
+// Struct for storing OHLC values.
+struct BarEntry {
+  datetime time;
+  double open, high, low, close;
+  // Struct constructor.
+  BarEntry() : open(0), high(0), low(0), close(0), time(0){};
+  BarEntry(double _open, double _high, double _low, double _close, datetime _time = 0)
+      : time(_time), open(_open), high(_high), low(_low), close(_close) {
+    if (_time == 0) {
+      _time = TimeCurrent();
+    }
+  }
+  // Struct methods.
+  // Getters
+  void GetValues(double& _out[]) {
+    ArrayResize(_out, 4);
+    int _index = ArraySize(_out) - 4;
+    _out[_index++] = open;
+    _out[_index++] = high;
+    _out[_index++] = low;
+    _out[_index++] = close;
+  }
+  // Serializers.
+  SerializerNodeType Serialize(Serializer& s) {
+    // s.Pass(this, "time", TimeToString(time));
+    s.Pass(this, "open", open);
+    s.Pass(this, "high", high);
+    s.Pass(this, "low", low);
+    s.Pass(this, "close", close);
+    return SerializerNodeObject;
+  }
+  // Converters.
+  string ToCSV() { return StringFormat("%d,%g,%g,%g,%g", time, open, high, low, close); }
+};
+
+// Struct for storing bar shape values.
+struct BarShape {
+  double body_size;    // Bar's body size (abs).
+  double candle_size;  // Bar's candle size (can be negative).
+  double head_size;    // Bar's head size.
+  double range_size;   // Bar's whole range size comparing to price.
+  double tail_size;    // Bar's tail size.
+  // Constructor.
+  BarShape() : body_size(0), candle_size(0), head_size(0), range_size(0), tail_size(0) {}
+  BarShape(double _bsp, double _csp, double _hsp, double _rsp, double _tsp) {
+    body_size = _bsp;
+    candle_size = _csp;
+    head_size = _hsp;
+    range_size = _rsp;
+    tail_size = _tsp;
+  };
+  // Getters.
+  double GetBodySize() { return body_size; }
+  double GetCandleSize() { return candle_size; }
+  double GetHeadSize() { return head_size; }
+  double GetRangeSize() { return range_size; }
+  double GetTailSize() { return tail_size; }
+  void GetValues(double& _out[]) {
+    ArrayResize(_out, 5);
+    int _index = ArraySize(_out) - 5;
+    _out[_index++] = body_size;
+    _out[_index++] = candle_size;
+    _out[_index++] = head_size;
+    _out[_index++] = range_size;
+    _out[_index++] = tail_size;
+  }
+  // Serializers.
+  SerializerNodeType Serialize(Serializer& s) {
+    // s.Pass(this, "time", TimeToString(time));
+    s.Pass(this, "body_size", body_size);
+    s.Pass(this, "candle_size", candle_size);
+    s.Pass(this, "head_size", head_size);
+    s.Pass(this, "range_size", range_size);
+    s.Pass(this, "tail_size", tail_size);
+    return SerializerNodeObject;
+  }
+  // Converters.
+  string ToCSV() { return StringFormat("%g,%g,%g,%g,%g", body_size, candle_size, head_size, range_size, tail_size); };
+};
--- Chart.struct.h
+++ Chart.struct.h
@@ -30,6 +30,7 @@ class Class;
 class Serializer;
 
 // Includes.
+#include "Bar.struct.h"
 #include "Chart.enum.h"
 #include "SerializerNode.enum.h"
 
@@ -46,86 +47,6 @@ struct ChartBarTime {
   static datetime Get(const string _symbol, const ENUM_TIMEFRAMES _tf, const int _shift) {
     return Chart::iTime(_symbol, _tf, _shift);
   }
-};
-
-// Struct for storing OHLC values.
-struct BarEntry {
-  datetime time;
-  double open, high, low, close;
-  // Struct constructor.
-  BarEntry() : open(0), high(0), low(0), close(0), time(0){};
-  BarEntry(double _open, double _high, double _low, double _close, datetime _time = 0)
-      : time(_time), open(_open), high(_high), low(_low), close(_close) {
-    if (_time == 0) {
-      _time = TimeCurrent();
-    }
-  }
-  // Struct methods.
-  // Getters
-  void GetValues(double& _out[]) {
-    ArrayResize(_out, 4);
-    int _index = ArraySize(_out) - 4;
-    _out[_index++] = open;
-    _out[_index++] = high;
-    _out[_index++] = low;
-    _out[_index++] = close;
-  }
-  // Serializers.
-  SerializerNodeType Serialize(Serializer& s) {
-    // s.Pass(this, "time", TimeToString(time));
-    s.Pass(this, "open", open);
-    s.Pass(this, "high", high);
-    s.Pass(this, "low", low);
-    s.Pass(this, "close", close);
-    return SerializerNodeObject;
-  }
-  // Converters.
-  string ToCSV() { return StringFormat("%d,%g,%g,%g,%g", time, open, high, low, close); }
-};
-
-// Struct for storing bar shape values.
-struct BarShape {
-  double body_size;    // Bar's body size (abs).
-  double candle_size;  // Bar's candle size (can be negative).
-  double head_size;    // Bar's head size.
-  double range_size;   // Bar's whole range size comparing to price.
-  double tail_size;    // Bar's tail size.
-  // Constructor.
-  BarShape() : body_size(0), candle_size(0), head_size(0), range_size(0), tail_size(0) {}
-  BarShape(double _bsp, double _csp, double _hsp, double _rsp, double _tsp) {
-    body_size = _bsp;
-    candle_size = _csp;
-    head_size = _hsp;
-    range_size = _rsp;
-    tail_size = _tsp;
-  };
-  // Getters.
-  double GetBodySize() { return body_size; }
-  double GetCandleSize() { return candle_size; }
-  double GetHeadSize() { return head_size; }
-  double GetRangeSize() { return range_size; }
-  double GetTailSize() { return tail_size; }
-  void GetValues(double& _out[]) {
-    ArrayResize(_out, 5);
-    int _index = ArraySize(_out) - 5;
-    _out[_index++] = body_size;
-    _out[_index++] = candle_size;
-    _out[_index++] = head_size;
-    _out[_index++] = range_size;
-    _out[_index++] = tail_size;
-  }
-  // Serializers.
-  SerializerNodeType Serialize(Serializer& s) {
-    // s.Pass(this, "time", TimeToString(time));
-    s.Pass(this, "body_size", body_size);
-    s.Pass(this, "candle_size", candle_size);
-    s.Pass(this, "head_size", head_size);
-    s.Pass(this, "range_size", range_size);
-    s.Pass(this, "tail_size", tail_size);
-    return SerializerNodeObject;
-  }
-  // Converters.
-  string ToCSV() { return StringFormat("%g,%g,%g,%g,%g", body_size, candle_size, head_size, range_size, tail_size); };
 };
 
 // Defines struct to store symbol data.
