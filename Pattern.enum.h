80c563d7b56fc2306a3a7173227649d1cc912324
kenorb <kenorb@users.noreply.github.com>
2021-03-20 21:52:16 +0000

    Pattern: Adds more 2 and 3 candle patterns

--- Pattern.enum.h
+++ Pattern.enum.h
@@ -27,27 +27,107 @@
 
 /* Enumeration for 2-candle patterns. */
 enum ENUM_PATTERN_2CANDLE {
-  PATTERN_2CANDLE_NONE = 0 << 0,                   // None
-  PATTERN_2CANDLE_BODY_GT_BODY = 1 << 0,           // Body size greater than the previous one.
-  PATTERN_2CANDLE_CLOSE_GT_CLOSE = 1 << 1,         // Close greater than the previous one.
-  PATTERN_2CANDLE_CLOSE_GT_HIGH = 1 << 2,          // Close greater than previous high.
-  PATTERN_2CANDLE_CLOSE_LT_LOW = 1 << 3,           // Close lower than previous low.
-  PATTERN_2CANDLE_HIGH_GT_HIGH = 1 << 4,           // High greater than the previous one.
-  PATTERN_2CANDLE_LOW_GT_LOW = 1 << 5,             // Low greater than the previous one.
-  PATTERN_2CANDLE_OPEN_GT_OPEN = 1 << 6,           // Open greater than the previous one.
-  PATTERN_2CANDLE_PP_GT_PP = 1 << 7,               // Pivot price greater than the previous one (HLC/3).
-  PATTERN_2CANDLE_PP_GT_PP_OPEN = 1 << 8,          // Pivot price open is greater than the previous one (OHLC/4).
-  PATTERN_2CANDLE_RANGE_DBL_RANGE = 1 << 9,        // Range size doubled from the previous one.
-  PATTERN_2CANDLE_RANGE_GT_RANGE = 1 << 10,        // Range greater than the previous one.
-  PATTERN_2CANDLE_WEIGHTED_GT_WEIGHTED = 1 << 11,  // Weighted price is greater than the previous one (OH2C/4).
-  PATTERN_2CANDLE_WICKS_DBL_WICKS = 1 << 12,       // Wicks size double from the previous onces.
-  PATTERN_2CANDLE_WICKS_GT_WICKS = 1 << 13,        // Wicks size greater than the previous onces.
+  PATTERN_2CANDLE_NONE = 0 << 0,                   // None.
+  PATTERN_2CANDLE_BEARS = 1 << 0,                  // Two bear candles.
+  PATTERN_2CANDLE_BODY_GT_BODY = 1 << 1,           // Body size is greater than the previous one.
+  PATTERN_2CANDLE_BULLS = 1 << 2,                  // Two bulls candles.
+  PATTERN_2CANDLE_CLOSE_GT_CLOSE = 1 << 3,         // Close is greater than the previous one.
+  PATTERN_2CANDLE_CLOSE_GT_HIGH = 1 << 4,          // Close is greater than previous high.
+  PATTERN_2CANDLE_CLOSE_LT_LOW = 1 << 5,           // Close is lower than previous low.
+  PATTERN_2CANDLE_HIGH_GT_HIGH = 1 << 6,           // High is greater than the previous one.
+  PATTERN_2CANDLE_HIGH_GT_HOC = 1 << 7,            // High is greater than the previous higher price (open or close).
+  PATTERN_2CANDLE_HOC_GT_HIGH = 1 << 8,            // Higher price (open or close) is greater than the previous high.
+  PATTERN_2CANDLE_HOC_GT_HOC = 1 << 9,             // Higher price (open or close) is greater than the previous one.
+  PATTERN_2CANDLE_LOC_LT_LOC = 1 << 10,            // Lower price (open or close) is lower than the previous one.
+  PATTERN_2CANDLE_LOC_LT_LOW = 1 << 11,            // Lower price (open or close) is lower than the previous low.
+  PATTERN_2CANDLE_LOW_LT_LOC = 1 << 12,            // Low is lower than the previous lower price (open or close).
+  PATTERN_2CANDLE_LOW_LT_LOW = 1 << 13,            // Low is lower than the previous one.
+  PATTERN_2CANDLE_OPEN_GT_OPEN = 1 << 14,          // Open is greater than the previous one.
+  PATTERN_2CANDLE_PP_GT_PP = 1 << 15,              // Pivot price is greater than the previous one (HLC/3).
+  PATTERN_2CANDLE_PP_GT_PP_OPEN = 1 << 16,         // Pivot price open is greater than the previous one (OHLC/4).
+  PATTERN_2CANDLE_RANGE_DBL_RANGE = 1 << 17,       // Range size doubled from the previous one.
+  PATTERN_2CANDLE_RANGE_GT_RANGE = 1 << 18,        // Range is greater than the previous one.
+  PATTERN_2CANDLE_RANGE_IN_BODY = 1 << 19,         // Range is inside body of the previous candle.
+  PATTERN_2CANDLE_RANGE_OUT_RANGE = 1 << 20,       // Range is outside of range of the previous candle.
+  PATTERN_2CANDLE_WEIGHTED_GT_WEIGHTED = 1 << 21,  // Weighted price is greater than the previous one (OH2C/4).
+  PATTERN_2CANDLE_WICKS_DBL_WICKS = 1 << 22,       // Size of wicks doubled from the previous onces.
+  PATTERN_2CANDLE_WICKS_GT_WICKS = 1 << 23,        // Size of wicks is greater than the previous onces.
+  // Patterns based on the existing patterns.
+  PATTERN_2CANDLE_BODY_IN_BODY = ~PATTERN_2CANDLE_HOC_GT_HOC | ~PATTERN_2CANDLE_LOC_LT_LOC,
+  PATTERN_2CANDLE_BODY_OUT_BODY = PATTERN_2CANDLE_HOC_GT_HOC | PATTERN_2CANDLE_LOC_LT_LOC,
+  PATTERN_2CANDLE_RANGE_IN_RANGE = ~PATTERN_2CANDLE_HIGH_GT_HIGH | ~PATTERN_2CANDLE_LOW_LT_LOW,
+  PATTERN_2CANDLE_RANGE_OUT_RANGE = PATTERN_2CANDLE_HIGH_GT_HIGH | PATTERN_2CANDLE_LOW_LT_LOW,
+  // Bearish engulfing pattern.
+  // A lot of momentum in favor of price falling.
+  PATTERN_2CANDLE_BEAR_ENGULFING = ~PATTERN_2CANDLE_BEARS | ~PATTERN_2CANDLE_BULLS | ~PATTERN_2CANDLE_CLOSE_GT_CLOSE |
+                                   PATTERN_2CANDLE_BODY_OUT_BODY | PATTERN_2CANDLE_RANGE_OUT_RANGE |
+                                   PATTERN_2CANDLE_HOC_GT_HOC | PATTERN_2CANDLE_LOC_LT_LOC,
+  // Bullish engulfing pattern.
+  // A lot of momentum in favor of price rising.
+  PATTERN_2CANDLE_BULL_ENGULFING = ~PATTERN_2CANDLE_BEARS | ~PATTERN_2CANDLE_BULLS | PATTERN_2CANDLE_CLOSE_GT_CLOSE |
+                                   PATTERN_2CANDLE_BODY_OUT_BODY | PATTERN_2CANDLE_RANGE_OUT_RANGE |
+                                   PATTERN_2CANDLE_HOC_GT_HOC | PATTERN_2CANDLE_LOC_LT_LOC,
+  // Body is inside of the previous candle's range.
+  PATTERN_2CANDLE_BODY_IN_RANGE = ~PATTERN_2CANDLE_HOC_GT_HIGH | ~PATTERN_2CANDLE_LOC_LT_LOW,
+  // Body is outside of the previous candle's range (partial Bullish engulfing).
+  PATTERN_2CANDLE_BODY_OUT_RANGE = PATTERN_2CANDLE_HOC_GT_HIGH | PATTERN_2CANDLE_LOC_LT_LOW,
+  // Range is inside of the previous candle's body (partial Harami pattern).
+  PATTERN_2CANDLE_RANGE_IN_BODY = ~PATTERN_2CANDLE_HIGH_GT_HOC | ~PATTERN_2CANDLE_LOW_LT_LOC,
+  // Range is outside of the previous candle's body (partial Harami pattern).
+  PATTERN_2CANDLE_RANGE_OUT_BODY = ~PATTERN_2CANDLE_HIGH_GT_HOC | ~PATTERN_2CANDLE_LOW_LT_LOC,
+  // Harami pattern.
+  // Neutral pattern where price is being pushed into a tighter range.
+  PATTERN_2CANDLE_HARAMI = PATTERN_2CANDLE_BODY_IN_BODY | PATTERN_2CANDLE_RANGE_IN_BODY | ~PATTERN_2CANDLE_HOC_GT_HOC |
+                           ~PATTERN_2CANDLE_LOC_LT_LOC,
+  // Dark cloud cover.
+  // Price acted like it was continuing upwards, but then reversed.
+  PATTERN_2CANDLE_DARK_CLOUD_COVER = ~PATTERN_2CANDLE_BEARS | ~PATTERN_2CANDLE_BULLS | ~PATTERN_2CANDLE_CLOSE_GT_CLOSE |
+                                     PATTERN_2CANDLE_HIGH_GT_HIGH | ~PATTERN_2CANDLE_LOW_LT_LOW |
+                                     PATTERN_2CANDLE_HOC_GT_HOC | ~PATTERN_2CANDLE_LOC_LT_LOC,
+  // Rising sun.
+  // Price looked like it was going to continue down, then moved up.
+  PATTERN_2CANDLE_RISING_SUN = ~PATTERN_2CANDLE_BEARS | ~PATTERN_2CANDLE_BULLS | ~PATTERN_2CANDLE_CLOSE_GT_CLOSE |
+                               PATTERN_2CANDLE_HIGH_GT_HIGH | ~PATTERN_2CANDLE_LOW_LT_LOW | PATTERN_2CANDLE_HOC_GT_HOC |
+                               ~PATTERN_2CANDLE_LOC_LT_LOC,
+  // @todo
   FINAL_ENUM_PATTERN_2CANDLE = INT_MAX
 };
 
 /* Enumeration for 3-candle patterns. */
 enum ENUM_PATTERN_3CANDLE {
-  PATTERN_3CANDLE_NONE = 0 << 0,  // None
+  PATTERN_3CANDLE_NONE = 0 << 0,              // None
+  PATTERN_3CANDLE_BEARS = 1 << 0,             // Three bear candles.
+  PATTERN_3CANDLE_BODY0_DBL_SUM = 1 << 1,     // Body size is greater than doubled sum of others.
+  PATTERN_3CANDLE_BODY0_GT_SUM = 1 << 2,      // Body size is greater than sum of others.
+  PATTERN_3CANDLE_BODY_DEC = 1 << 3,          // Body size decreases.
+  PATTERN_3CANDLE_BODY_INC = 1 << 4,          // Body size increases.
+  PATTERN_3CANDLE_BULLS = 1 << 5,             // Three bull candles.
+  PATTERN_3CANDLE_CLOSE_DEC = 1 << 6,         // Close price decreases.
+  PATTERN_3CANDLE_CLOSE_INC = 1 << 7,         // Close price increases.
+  PATTERN_3CANDLE_HIGH0_LT_LOW2 = 1 << 8,     // High price lower than low price of 2 bars before.
+  PATTERN_3CANDLE_HIGH1_LT_PP = 1 << 9,       // High price of the middle bar is lower than pivot price.
+  PATTERN_3CANDLE_HIGH_DEC = 1 << 10,         // High price decreases.
+  PATTERN_3CANDLE_HIGH_INC = 1 << 11,         // High price increases.
+  PATTERN_3CANDLE_LOW0_GT_HIGH2 = 1 << 12,    // Low price is greater than high price of 2 bars before.
+  PATTERN_3CANDLE_LOW1_GT_PP = 1 << 13,       // Low price of the middle bar is greater than pivot price.
+  PATTERN_3CANDLE_LOW_DEC = 1 << 14,          // Low price decreases.
+  PATTERN_3CANDLE_LOW_INC = 1 << 15,          // Low price increases.
+  PATTERN_3CANDLE_OPEN0_GT_HIGH2 = 1 << 16,   // Open price is greater than high price of 2 bars before.
+  PATTERN_3CANDLE_OPEN0_LT_LOW2 = 1 << 17,    // Open price is lower than low price of 2 bars before.
+  PATTERN_3CANDLE_OPEN_DEC = 1 << 18,         // Open price decreases.
+  PATTERN_3CANDLE_OPEN_INC = 1 << 19,         // Open price increases.
+  PATTERN_3CANDLE_PEAK = 1 << 20,             // High or low price at peak.
+  PATTERN_3CANDLE_PP_DEC = 1 << 21,           // Pivot point decreases.
+  PATTERN_3CANDLE_PP_INC = 1 << 22,           // Pivot point increases.
+  PATTERN_3CANDLE_RANGE0_GT_SUM = 1 << 23,    // Range size is greater than sum of others.
+  PATTERN_3CANDLE_RANGE1_GT_SUM = 1 << 24,    // Range size of middle candle is greater than sum of others.
+  PATTERN_3CANDLE_RANGE_DEC = 1 << 25,        // Range size decreases.
+  PATTERN_3CANDLE_RANGE_INC = 1 << 26,        // Range size increases.
+  PATTERN_3CANDLE_WICKS0_DBL_SUM = 1 << 27,   // Size of wicks are greater than doubled sum of others.
+  PATTERN_3CANDLE_WICKS0_GT_BODY = 1 << 28,   // Size of wicks are greater than sum of bodies.
+  PATTERN_3CANDLE_WICKS0_GT_SUM = 1 << 29,    // Size of wicks are greater than sum of others.
+  PATTERN_3CANDLE_WICKS1_DBL_BODY = 1 << 30,  // Size of middle wicks are greater than doubled sum of bodies.
+  PATTERN_3CANDLE_WICKS1_GT_BODY = 1 << 31,   // Size of middle wicks are greater than sum of bodies.
   FINAL_ENUM_PATTERN_3CANDLE = INT_MAX
 };
 
--- Pattern.struct.h
+++ Pattern.struct.h
@@ -30,46 +30,142 @@
 #include "Pattern.enum.h"
 
 struct PatternEntry {
-  int candle[7];
+  unsigned int pattern[8];
   // Struct constructor.
   PatternEntry(BarOHLC _ohlc[]) {
-    // 2-candle patterns.
+    // 1-candle patterns.
+    BarEntry _candle0(_ohlc[0]);
+    pattern[0] = _candle0.pattern.GetPattern();
+    // Calculates 2-candle patterns.
     if (ArraySize(_ohlc) > 1) {
-      // Body size greater than the previous one.
+      // Two bear candles.
+      SetPattern(PATTERN_2CANDLE_BEARS, _ohlc[0].open > _ohlc[0].close && _ohlc[1].open > _ohlc[1].close);
+      // Body size is greater than the previous one.
       SetPattern(PATTERN_2CANDLE_BODY_GT_BODY, _ohlc[0].GetBodyAbs() > _ohlc[1].GetBodyAbs());
-      // Close price greater than the previous one.
+      // Two bulls candles.
+      SetPattern(PATTERN_2CANDLE_BULLS, _ohlc[0].open < _ohlc[0].close && _ohlc[1].open < _ohlc[1].close);
+      // Close price is greater than the previous one.
       SetPattern(PATTERN_2CANDLE_CLOSE_GT_CLOSE, _ohlc[0].close > _ohlc[1].close);
-      // Close price greater than previous high.
+      // Close price is greater than previous high.
       SetPattern(PATTERN_2CANDLE_CLOSE_GT_HIGH, _ohlc[0].close > _ohlc[1].high);
-      // Close price lower than previous low.
+      // Close price is lower than previous low.
       SetPattern(PATTERN_2CANDLE_CLOSE_LT_LOW, _ohlc[0].close < _ohlc[1].low);
-      // High price greater than the previous one.
+      // Higher price (open or close) is greater than the previous high.
+      SetPattern(PATTERN_2CANDLE_HOC_GT_HIGH, _ohlc[0].GetMaxOC() > _ohlc[1].high);
+      // Higher price (open or close) is greater than the previous one.
+      SetPattern(PATTERN_2CANDLE_HOC_GT_HOC, _ohlc[0].GetMaxOC() > _ohlc[1].GetMaxOC());
+      // High price is greater than the previous one.
       SetPattern(PATTERN_2CANDLE_HIGH_GT_HIGH, _ohlc[0].high > _ohlc[1].high);
-      // Low price greater than the previous one.
-      SetPattern(PATTERN_2CANDLE_LOW_GT_LOW, _ohlc[0].low > _ohlc[1].low);
-      // Open price greater than the previous one.
+      // High is greater than the previous higher price (open or close).
+      SetPattern(PATTERN_2CANDLE_HIGH_GT_HOC, _ohlc[0].high > _ohlc[1].GetMaxOC());
+      // Lower price (open or close) is lower than the previous one.
+      SetPattern(PATTERN_2CANDLE_LOC_LT_LOC, _ohlc[0].GetMinOC() < _ohlc[1].GetMinOC());
+      // Lower price (open or close) is lower than the previous low.
+      SetPattern(PATTERN_2CANDLE_LOC_LT_LOW, _ohlc[0].GetMinOC() < _ohlc[1].low);
+      // Low is lower than the previous lower price (open or close).
+      SetPattern(PATTERN_2CANDLE_LOW_LT_LOC, _ohlc[0].low < _ohlc[1].GetMinOC());
+      // Low price is lower than the previous one.
+      SetPattern(PATTERN_2CANDLE_LOW_LT_LOW, _ohlc[0].low < _ohlc[1].low);
+      // Open price is greater than the previous one.
       SetPattern(PATTERN_2CANDLE_OPEN_GT_OPEN, _ohlc[0].open > _ohlc[1].open);
-      // Pivot price greater than the previous one (HLC/3).
+      // Pivot price is greater than the previous one (HLC/3).
       SetPattern(PATTERN_2CANDLE_PP_GT_PP, _ohlc[0].GetPivot() > _ohlc[1].GetPivot());
       // Pivot price open is greater than the previous one (OHLC/4).
       SetPattern(PATTERN_2CANDLE_PP_GT_PP_OPEN, _ohlc[0].GetPivot() > _ohlc[1].GetPivotWithOpen(_ohlc[1].open));
       // Range size doubled from the previous one.
       SetPattern(PATTERN_2CANDLE_RANGE_DBL_RANGE, _ohlc[0].GetRange() > _ohlc[1].GetRange() * 2);
-      // Range greater than the previous one.
+      // Range is greater than the previous one.
       SetPattern(PATTERN_2CANDLE_RANGE_GT_RANGE, _ohlc[0].GetRange() > _ohlc[1].GetRange());
       // Weighted price is greater than the previous one (OH2C/4).
       SetPattern(PATTERN_2CANDLE_WEIGHTED_GT_WEIGHTED, _ohlc[0].GetWeighted() > _ohlc[1].GetWeighted());
-      // Wicks size double from the previous onces.
+      // Size of wicks doubled from the previous onces.
       SetPattern(PATTERN_2CANDLE_WICKS_DBL_WICKS, _ohlc[0].GetWickSum() > _ohlc[1].GetWickSum() * 2);
-      // Wicks size greater than the previous onces.
+      // Size of wicks is greater than the previous onces.
       SetPattern(PATTERN_2CANDLE_WICKS_DBL_WICKS, _ohlc[0].GetWickSum() > _ohlc[1].GetWickSum());
+    }
+    // Calculates 3-candle patterns.
+    if (ArraySize(_ohlc) > 2) {
+      // Three bear candles.
+      SetPattern(PATTERN_3CANDLE_BEARS, CheckPattern(PATTERN_2CANDLE_BEARS, 1) && _ohlc[2].open > _ohlc[2].close);
+      // Body size is greater than doubled sum of others.
+      SetPattern(PATTERN_3CANDLE_BODY0_DBL_SUM,
+                 _ohlc[0].GetBodyAbs() > (_ohlc[1].GetBodyAbs() + _ohlc[2].GetBodyAbs()) * 2);
+      // Body size is greater than sum of others.
+      SetPattern(PATTERN_3CANDLE_BODY0_GT_SUM, _ohlc[0].GetBodyAbs() > _ohlc[1].GetBodyAbs() + _ohlc[2].GetBodyAbs());
+      // Body size decreases.
+      SetPattern(PATTERN_3CANDLE_BODY_DEC,
+                 _ohlc[0].GetBodyAbs() < _ohlc[1].GetBodyAbs() && _ohlc[1].GetBodyAbs() < _ohlc[2].GetBodyAbs());
+      // Body size increases.
+      SetPattern(PATTERN_3CANDLE_BODY_INC,
+                 _ohlc[0].GetBodyAbs() > _ohlc[1].GetBodyAbs() && _ohlc[1].GetBodyAbs() > _ohlc[2].GetBodyAbs());
+      // Three bull candles.
+      SetPattern(PATTERN_3CANDLE_BULLS, CheckPattern(PATTERN_2CANDLE_BULLS, 1) && _ohlc[2].open < _ohlc[2].close);
+      // Close price decreases.
+      SetPattern(PATTERN_3CANDLE_CLOSE_DEC, _ohlc[0].close < _ohlc[1].close && _ohlc[1].close < _ohlc[2].close);
+      // Close price increases.
+      SetPattern(PATTERN_3CANDLE_CLOSE_INC, _ohlc[0].close > _ohlc[1].close && _ohlc[1].close > _ohlc[2].close);
+      // High price lower than low price of 2 bars before.
+      SetPattern(PATTERN_3CANDLE_HIGH0_LT_LOW2, _ohlc[0].high < _ohlc[2].low);
+      // High price of the middle bar is lower than pivot price.
+      SetPattern(PATTERN_3CANDLE_HIGH1_LT_PP, _ohlc[1].high < fmin(_ohlc[0].GetPivot(), _ohlc[2].GetPivot()));
+      // High price decreases.
+      SetPattern(PATTERN_3CANDLE_HIGH_DEC, _ohlc[0].high < _ohlc[1].high && _ohlc[1].high < _ohlc[2].high);
+      // High price increases.
+      SetPattern(PATTERN_3CANDLE_HIGH_INC, _ohlc[0].high > _ohlc[1].high && _ohlc[1].high > _ohlc[2].high);
+      // Low price is greater than high price of 2 bars before.
+      SetPattern(PATTERN_3CANDLE_LOW0_GT_HIGH2, _ohlc[0].low > _ohlc[2].high);
+      // Low price of the middle bar is greater than pivot price.
+      SetPattern(PATTERN_3CANDLE_LOW1_GT_PP, _ohlc[1].low > fmax(_ohlc[0].GetPivot(), _ohlc[2].GetPivot()));
+      // Low price decreases.
+      SetPattern(PATTERN_3CANDLE_LOW_DEC, _ohlc[0].low < _ohlc[1].low && _ohlc[1].low < _ohlc[2].low);
+      // Low price increases.
+      SetPattern(PATTERN_3CANDLE_LOW_INC, _ohlc[0].low > _ohlc[1].low && _ohlc[1].low > _ohlc[2].low);
+      // Open price is greater than high price of 2 bars before.
+      SetPattern(PATTERN_3CANDLE_OPEN0_GT_HIGH2, _ohlc[0].open > _ohlc[2].high);
+      // Open price is lower than low price of 2 bars before.
+      SetPattern(PATTERN_3CANDLE_OPEN0_LT_LOW2, _ohlc[0].open < _ohlc[2].low);
+      // Open price decreases.
+      SetPattern(PATTERN_3CANDLE_OPEN_DEC, _ohlc[0].open < _ohlc[1].open && _ohlc[1].open < _ohlc[2].open);
+      // Open price increases.
+      SetPattern(PATTERN_3CANDLE_OPEN_INC, _ohlc[0].open > _ohlc[1].open && _ohlc[1].open > _ohlc[2].open);
+      // High or low price at peak.
+      SetPattern(PATTERN_3CANDLE_PEAK,
+                 _ohlc[0].high > fmax(_ohlc[1].high, _ohlc[2].high) || _ohlc[0].low < fmin(_ohlc[1].low, _ohlc[2].low));
+      // Pivot point decreases.
+      SetPattern(PATTERN_3CANDLE_PP_DEC,
+                 _ohlc[0].GetPivot() < _ohlc[1].GetPivot() && _ohlc[1].GetPivot() < _ohlc[2].GetPivot());
+      // Pivot point increases.
+      SetPattern(PATTERN_3CANDLE_PP_INC,
+                 _ohlc[0].GetPivot() > _ohlc[1].GetPivot() && _ohlc[1].GetPivot() > _ohlc[2].GetPivot());
+      // Range size is greater than sum of others.
+      SetPattern(PATTERN_3CANDLE_RANGE0_GT_SUM, _ohlc[0].GetRange() > (_ohlc[1].GetRange() + _ohlc[2].GetRange()));
+      // Range size of middle candle is greater than sum of others.
+      SetPattern(PATTERN_3CANDLE_RANGE1_GT_SUM, _ohlc[1].GetRange() > (_ohlc[0].GetRange() + _ohlc[2].GetRange()));
+      // Range size decreases.
+      SetPattern(PATTERN_3CANDLE_RANGE_DEC,
+                 _ohlc[0].GetRange() < _ohlc[1].GetRange() && _ohlc[1].GetRange() < _ohlc[2].GetRange());
+      // Range size increases.
+      SetPattern(PATTERN_3CANDLE_RANGE_INC,
+                 _ohlc[0].GetRange() > _ohlc[1].GetRange() && _ohlc[1].GetRange() > _ohlc[2].GetRange());
+      // Size of wicks are greater than doubled sum of others.
+      SetPattern(PATTERN_3CANDLE_WICKS0_DBL_SUM,
+                 _ohlc[0].GetWickSum() > (_ohlc[1].GetWickSum() + _ohlc[2].GetWickSum()) * 2);
+      // Size of wicks are greater than sum of bodies.
+      SetPattern(PATTERN_3CANDLE_WICKS0_GT_BODY, _ohlc[0].GetWickSum() > _ohlc[1].GetBodyAbs() + _ohlc[2].GetBodyAbs());
+      // Size of wicks are greater than sum of others.
+      SetPattern(PATTERN_3CANDLE_WICKS0_GT_SUM, _ohlc[0].GetWickSum() > _ohlc[1].GetWickSum() + _ohlc[2].GetWickSum());
+      // Size of middle wicks are greater than doubled sum of bodies.
+      SetPattern(PATTERN_3CANDLE_WICKS1_DBL_BODY,
+                 _ohlc[1].GetWickSum() > (_ohlc[0].GetBodyAbs() + _ohlc[2].GetBodyAbs()) * 2);
+      // Size of middle wicks are greater than sum of bodies.
+      SetPattern(PATTERN_3CANDLE_WICKS1_GT_BODY, _ohlc[1].GetWickSum() > _ohlc[0].GetBodyAbs() + _ohlc[2].GetBodyAbs());
     }
   }
   // Struct methods for bitwise operations.
-  bool CheckPattern(int _flags, int _index) { return (candle[_index] & _flags) != 0; }
-  bool CheckPatternsAll(int _flags, int _index) { return (candle[_index] & _flags) == _flags; }
-  void AddPattern(int _flags, int _index) { candle[_index] |= _flags; }
-  void RemovePattern(int _flags, int _index) { candle[_index] &= ~_flags; }
+  bool CheckPattern(int _flags, int _index) { return (pattern[_index] & _flags) != 0; }
+  bool CheckPatternsAll(int _flags, int _index) { return (pattern[_index] & _flags) == _flags; }
+  void AddPattern(int _flags, int _index) { pattern[_index] |= _flags; }
+  void RemovePattern(int _flags, int _index) { pattern[_index] &= ~_flags; }
   void SetPattern(ENUM_BAR_PATTERN _flag, bool _value = true) {
     if (_value) {
       AddPattern(_flag, 0);
@@ -119,5 +215,5 @@ struct PatternEntry {
       RemovePattern(_flag, 6);
     }
   }
-  void SetPattern(int _flags, int _index) { candle[_index] = _flags; }
+  void SetPattern(int _flags, int _index) { pattern[_index] = _flags; }
 };
