66beaaa5fdc215bd5b91d09f8453d7eb5cdc1548
kenorb <kenorb@users.noreply.github.com>
2021-05-04 19:29:41 +0100

    Chart: Adds ENUM_APPLIED_PRICE

--- Chart.enum.h
+++ Chart.enum.h
@@ -30,6 +30,19 @@
 #pragma once
 #endif
 
+#ifndef __MQL__
+// Defines enumeration for price price base calculations.
+enum ENUM_APPLIED_PRICE {
+  PRICE_CLOSE = 0,     // Close price.
+  PRICE_OPEN = 1,      // Open price.
+  PRICE_HIGH = 2,      // The maximum price for the period.
+  PRICE_LOW = 3,       // The minimum price for the period.
+  PRICE_MEDIAN = 4,    // Median price, (high + low)/2.
+  PRICE_TYPICAL = 5,   // Typical price, (high + low + close)/3.
+  PRICE_WEIGHTED = 6,  // Weighted close price, (high + low + close + close)/4.
+};
+#endif
+
 // Defines enumeration for chart parameters.
 enum ENUM_CHART_PARAM {
   CHART_PARAM_NONE = 0,  // None
