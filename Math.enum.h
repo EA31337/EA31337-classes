f0c305eed66a93ea2af261b4851ac11c2cce6479
kenorb <kenorb@users.noreply.github.com>
2021-05-04 18:30:27 +0100

    Removes redefinition of ENUM_DATATYPE

--- Math.enum.h
+++ Math.enum.h
@@ -33,31 +33,6 @@
 #ifndef MATH_ENUM_H
 #define MATH_ENUM_H
 
-#ifndef __MQLBUILD__
-/**
- * Data type identifiers.
- *
- * @see:
- * - https://www.mql5.com/en/docs/constants/indicatorconstants/enum_datatype
- */
-enum ENUM_DATATYPE {
-  TYPE_BOOL,
-  TYPE_CHAR,
-  TYPE_UCHAR,
-  TYPE_SHORT,
-  TYPE_USHORT,
-  TYPE_COLOR,
-  TYPE_INT,
-  TYPE_UINT,
-  TYPE_DATETIME,
-  TYPE_LONG,
-  TYPE_ULONG,
-  TYPE_FLOAT,
-  TYPE_DOUBLE,
-  TYPE_STRING
-};
-#endif
-
 /* Enumeration for Math conditions. */
 enum ENUM_MATH_CONDITION {
   MATH_COND_EQ = 1,  // Argument values are equal.
