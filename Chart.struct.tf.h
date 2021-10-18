1c737b2ac303ec0f61a88732bf96b00e8445d0a9
kenorb <kenorb@users.noreply.github.com>
2021-10-18 21:27:23 +0100

    ChartTf: TfToString: Improves performance

--- Chart.struct.tf.h
+++ Chart.struct.tf.h
@@ -222,7 +222,56 @@ struct ChartTf {
    * Returns string representation of the timeframe.
    */
   static string TfToString(const ENUM_TIMEFRAMES _tf) {
-    return StringSubstr(EnumToString((_tf == 0 || _tf == PERIOD_CURRENT ? (ENUM_TIMEFRAMES)_Period : _tf)), 7);
+    switch (_tf) {
+      case PERIOD_CURRENT:
+        return "Current";
+      case PERIOD_M1:
+        return "M1";
+      case PERIOD_M2:
+        return "M2";
+      case PERIOD_M3:
+        return "M3";
+      case PERIOD_M4:
+        return "M4";
+      case PERIOD_M5:
+        return "M5";
+      case PERIOD_M6:
+        return "M6";
+      case PERIOD_M10:
+        return "M10";
+      case PERIOD_M12:
+        return "M12";
+      case PERIOD_M15:
+        return "M15";
+      case PERIOD_M20:
+        return "M20";
+      case PERIOD_M30:
+        return "M30";
+      case PERIOD_H1:
+        return "H1";
+      case PERIOD_H2:
+        return "H2";
+      case PERIOD_H3:
+        return "H3";
+      case PERIOD_H4:
+        return "H4";
+      case PERIOD_H6:
+        return "H6";
+      case PERIOD_H8:
+        return "H8";
+      case PERIOD_H12:
+        return "H12";
+      case PERIOD_D1:
+        return "D1";
+      case PERIOD_W1:
+        return "W1";
+      case PERIOD_MN1:
+        return "MN1";
+      default:
+        break;
+    }
+    SetUserError(ERR_INVALID_PARAMETER);
+    return "Unknown";
   }
 
   /**
