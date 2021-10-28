6b147aea4f940a055ae95fa644ef3d01b36effca
kenorb <kenorb@users.noreply.github.com>
2021-10-28 23:15:00 +0100

    Adds TickManager

---
+++ Tick/TickManager.h
@@ -0,0 +1,76 @@
+//+------------------------------------------------------------------+
+//|                                                EA31337 framework |
+//|                                 Copyright 2016-2021, EA31337 Ltd |
+//|                                       https://github.com/EA31337 |
+//+------------------------------------------------------------------+
+
+/*
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * @file
+ * Implements TickManager class.
+ */
+
+// Includes.
+#include "../BufferStruct.mqh"
+#include "TickManager.h"
+//#include "TickManager.struct.h"
+
+/**
+ * Class to store and manage tick data.
+ */
+class TickManager : public BufferStruct<MqlTick> {
+ protected:
+  /* Protected methods */
+
+  /**
+   * Init code (called on constructor).
+   */
+  void Init() {
+    AddFlags(DICT_FLAG_FILL_HOLES_UNSORTED);
+    SetMaxConflicts(86400);
+  }
+
+ public:
+  /**
+   * Default class constructor.
+   */
+  TickManager() { Init(); }
+
+  /**
+   * Class constructor with parameters.
+   */
+  // TickManager(TickManagerParams &_params) : params(_params) { Init(); }
+
+  /* Getters */
+
+  /**
+   * Gets a property value.
+   */
+  // template <typename T>
+  // T Get(STRUCT_ENUM(TickManagerParams, ENUM_TSM_PARAMS_PROP) _prop)
+  // { return params.Get<T>(_prop); }
+
+  /* Setters */
+
+  /**
+   * Sets a property value.
+   */
+  // template <typename T>
+  // void Set(STRUCT_ENUM(TickManagerParams, ENUM_TSM_PARAMS_PROP) _prop, T _value)
+  // { params.Set<T>(_prop, _value); }
+};
