a7b2e226471f674df38f2068d679048080ee5c22
kenorb <kenorb@users.noreply.github.com>
2023-07-08 00:58:25 +0100

    Trade: Adds MqlTradeRequest, MqlTradeResult and MqlTradeTransaction structures

--- Trade.struct.h
+++ Trade.struct.h
@@ -37,6 +37,67 @@ struct TradeStats;
 #include "DateTime.mqh"
 #include "Trade.enum.h"
 
+#ifndef __MQL__
+
+// Defines Trade Request structure.
+// @see: https://www.mql5.com/en/docs/constants/structures/mqltraderequest
+struct MqlTradeRequest {
+  ENUM_TRADE_REQUEST_ACTIONS action;     // Trade operation type
+  ulong magic;                           // Expert Advisor ID (magic number)
+  ulong order;                           // Order ticket
+  string symbol;                         // Trade symbol
+  double volume;                         // Requested volume for a deal in lots
+  double price;                          // Price
+  double stoplimit;                      // StopLimit level of the order
+  double sl;                             // Stop Loss level of the order
+  double tp;                             // Take Profit level of the order
+  ulong deviation;                       // Maximal possible deviation from the requested price
+  ENUM_ORDER_TYPE type;                  // Order type
+  ENUM_ORDER_TYPE_FILLING type_filling;  // Order execution type
+  ENUM_ORDER_TYPE_TIME type_time;        // Order expiration type
+  datetime expiration;                   // Order expiration time (for the orders of ORDER_TIME_SPECIFIED type)
+  string comment;                        // Order comment
+  ulong position;                        // Position ticket
+  ulong position_by;                     // The ticket of an opposite position
+};
+
+// Defines Trade Request Result structure.
+// @see: https://www.mql5.com/en/docs/constants/structures/mqltraderesult
+struct MqlTradeResult {
+  uint retcode;     // Operation return code
+  ulong deal;       // Deal ticket, if it is performed
+  ulong order;      // Order ticket, if it is placed
+  double volume;    // Deal volume, confirmed by broker
+  double price;     // Deal price, confirmed by broker
+  double bid;       // Current Bid price
+  double ask;       // Current Ask price
+  string comment;   // Broker comment to operation (by default it is filled by description of trade server return code)
+  uint request_id;  // Request ID set by the terminal during the dispatch
+  int retcode_external;  // Return code of an external trading system
+};
+
+// Defines Trade Transaction structure.
+// @see: https://www.mql5.com/en/docs/constants/structures/mqltradetransaction
+struct MqlTradeTransaction {
+  ulong deal;                        // Deal ticket
+  ulong order;                       // Order ticket
+  string symbol;                     // Trade symbol name
+  ENUM_TRADE_TRANSACTION_TYPE type;  // Trade transaction type
+  ENUM_ORDER_TYPE order_type;        // Order type
+  ENUM_ORDER_STATE order_state;      // Order state
+  ENUM_DEAL_TYPE deal_type;          // Deal type
+  ENUM_ORDER_TYPE_TIME time_type;    // Order type by action period
+  datetime time_expiration;          // Order expiration time
+  double price;                      // Price
+  double price_trigger;              // Stop limit order activation price
+  double price_sl;                   // Stop Loss level
+  double price_tp;                   // Take Profit level
+  double volume;                     // Volume in lots
+  ulong position;                    // Position ticket
+  ulong position_by;                 // Ticket of an opposite position
+};
+#endif
+
 /* Structure for trade parameters. */
 struct TradeParams {
   float lot_size;        // Default lot size.
@@ -60,7 +121,13 @@ struct TradeParams {
     SetLimits(0);
   }
   TradeParams(unsigned long _magic_no, ENUM_LOG_LEVEL _ll = V_INFO)
-      : bars_min(100), lot_size(0), order_comment(""), log_level(_ll), magic_no(_magic_no), risk_margin(1.0f), slippage(0) {}
+      : bars_min(100),
+        lot_size(0),
+        order_comment(""),
+        log_level(_ll),
+        magic_no(_magic_no),
+        risk_margin(1.0f),
+        slippage(0) {}
   TradeParams(TradeParams &_tparams) { this = _tparams; }
   // Deconstructor.
   ~TradeParams() {}
